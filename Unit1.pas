6b714916786f041a7316e107e4d49509e55565b1
Zhbert <zhbert@yandex.ru>
2020-01-31 21:56:14 +0300

    Initial commit

---
+++ Unit1.pas
@@ -0,0 +1,33 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Form1.Label1.Caption := Form1.Label1.Caption + ' ' + IntToStr(Screen.Height);
+  Form1.Label2.Caption := Form1.Label2.Caption + ' ' + IntToStr(Screen.Width);
+end;
+
+end.
